dbe4c9bc7127d41c9d9e9d7f0274d6545e7b08b1
EkhlasMridhaShimul <ekhlas29@student.sust.edu>
2020-07-16 15:34:28 +0600

    routing error & redirection page

--- src/app/root-routing/root-routing.module.ts
+++ src/app/root-routing/root-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('../app-redirection-page/app-redirection-page.module').then(
+        (m) => m.AppRedirectionPageModule
+      ),
+  },
+];
 @NgModule({
   declarations: [],
   imports: [CommonModule, RouterModule.forRoot(routes)],

02e5f692ba8309b383dec6d7ef0dc1b328d37ec0
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2020-10-31 13:30:23 +0600

    file cleaned

--- src/app/root-routing/RT-route-system/root.routes.ts
+++ src/app/root-routing/RT-route-system/root.routes.ts
@@ -1,24 +1,5 @@
 import { Routes } from '@angular/router';
-import * as fullPage from '../full-page.routes';
-import * as business from '../app.routes';
-import { NonavLayoutComponent } from '../../root/components/nonav-layout/nonav-layout.component';
-import { PageLayoutComponent } from '../../root/components/page-layout/page-layout.component';
 import { AuthGuardService } from 'src/app/shared-services/route-guards/auth-guard.service';
-import { SidenavLayoutComponent } from 'src/app/root/components/sidenav-layout/sidenav-layout.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: SidenavLayoutComponent,
-    children: business.getBusinessRoutes(),
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: '',
-    component: NonavLayoutComponent,
-    children: fullPage.getFullPageLayoutRoute(),
-  },
-];
 
 const routes2: Routes = [
   // {
--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -2,28 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER, PLATFORM_ID } from '@angular/core';
 import { RootRoutingModule } from '../root-routing/RT-route-system/root-routing.module';
 import { AppComponent } from './components/root-component/app.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppContentComponent } from './components/app-content/app-content.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { PageLayoutComponent } from './components/page-layout/page-layout.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { DomainService } from '../shared-services/utilities/domain.service';
 import { AppLoaderModule } from '../app-tools/app-loader/app-loader.module';
-import { NonavLayoutComponent } from './components/nonav-layout/nonav-layout.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { interceptorProvider } from '../shared-services/interceptors/interceptor.provider';
-import { ApiInterceptorService } from '../shared-services/interceptors/api-interceptor.service';
 import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
-import { MatIconModule } from '@angular/material/icon';
 import { ToastrModule } from 'ngx-toastr';
-import { SidenavLayoutComponent } from './components/sidenav-layout/sidenav-layout.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 
 export function initializer(domainService: DomainService) {
   return () => {
@@ -38,28 +24,15 @@ export function initializer(domainService: DomainService) {
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
-    AppContentComponent,
-    PageLayoutComponent,
-    NonavLayoutComponent,
     MainLayoutComponent,
     NotFoundComponent,
-    SidenavLayoutComponent,
   ],
   imports: [
     BrowserModule,
     RootRoutingModule,
-    HttpClientModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    FlexLayoutModule,
-    MatButtonModule,
     AppLoaderModule,
-    MatIconModule,
     ToastrModule.forRoot(),
-    LayoutModule,
-    MatSidenavModule,
-    MatListModule,
   ],
   providers: [
     JwtHelperService,

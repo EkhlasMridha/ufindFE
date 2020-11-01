95b3702e8eda70d756d2f7be53f1e94a6bf10806
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2020-11-01 10:19:22 +0600

    code cleaned

--- src/app/layouts/no-nav/no-nav.module.ts
+++ src/app/layouts/no-nav/no-nav.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { NoNavComponent } from './components/no-nav/no-nav.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentComponent } from './components/content/content.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import * as fullpage from "../../root-routing/full-page.routes";
 
 const routes: Routes = [
@@ -15,8 +13,6 @@ const routes: Routes = [
   declarations: [NoNavComponent, ContentComponent],
   imports: [
     CommonModule,
-    MatIconModule,
-    MatButtonModule,
     RouterModule.forChild(routes)
   ]
 })
--- src/app/layouts/top-nav/top-nav.module.ts
+++ src/app/layouts/top-nav/top-nav.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
-import { MatIconModule } from '@angular/material/icon';
 import { ToolbarModule } from '../toolbar/toolbar.module';
-import { MatButtonModule } from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentComponent } from './components/content/content.component';
 import * as nav from "../../root-routing/app.routes"
@@ -17,10 +14,7 @@ const routes: Routes = [
   declarations: [TopNavComponent, ContentComponent],
   imports: [
     CommonModule,
-    MatIconModule,
     ToolbarModule,
-    MatButtonModule,
-    FlexLayoutModule,
     RouterModule.forChild(routes)
   ],
 })

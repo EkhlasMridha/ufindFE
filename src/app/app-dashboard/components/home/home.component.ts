527120c5e1bbd186ba2a0ed1f1c561de6fcdbf64
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2021-02-07 16:38:42 +0600

    code structure

--- src/app/app-dashboard/components/home/home.component.ts
+++ src/app/app-dashboard/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TesService } from '../../services/tes.service';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +7,7 @@ import { TesService } from '../../services/tes.service';
 })
 export class HomeComponent implements OnInit {
   modalRef: any;
-  constructor (
-    private testService: TesService,
-  ) {
-    this.testService.getData().subscribe((res) => {
-      console.log('result');
-      console.log(res);
-    });
-  }
+  constructor () { }
 
   ngOnInit(): void { }
 }
--- src/app/layouts/toolbar/components/toolbar/toolbar.component.ts
+++ src/app/layouts/toolbar/components/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { IconService } from '@core/icon.service';
 import { TokenService } from '@core/token.service';
+import { UserManagerService } from '@core/user-manager.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -11,22 +12,31 @@ import { TokenService } from '@core/token.service';
 export class ToolbarComponent implements OnInit {
   @Input() showMenuButton: boolean;
   @Output() menuButton: EventEmitter<MouseEvent> = new EventEmitter();
+
+  profile: any;
   constructor (
-    private tokenService: TokenService,
     private router: Router,
-    private iconService: IconService
+    private iconService: IconService,
+    private userService: UserManagerService
   ) {
     this.iconService.loadIcons(['signout']);
+    this.getProfile();
   }
 
   ngOnInit(): void { }
 
   signoutUser() {
-    this.tokenService.removeToken();
+    this.userService.logout();
   }
 
   openDrawer(event) {
     this.menuButton.emit(event);
   }
 
+  getProfile() {
+    this.userService.getProfile().subscribe(res => {
+      console.log(res);
+      this.profile = res;
+    });
+  }
 }

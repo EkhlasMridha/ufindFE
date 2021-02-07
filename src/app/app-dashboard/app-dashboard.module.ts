199215ba840ef0c2123ea4c25184e853ad385303
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2021-02-07 16:24:26 +0600

    tsconfig mod

--- src/app/app-dashboard/app-dashboard.module.ts
+++ src/app/app-dashboard/app-dashboard.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { Routes, RouterModule } from '@angular/router';
-import { SharedMaterialModule } from '../shared-modules/shared-materials/shared-material/shared-material.module';
+import { SharedMaterialModule } from '@material/shared-material.module';
 import { ConfirmationStatusModalModule } from '../shared-modules/confirmation-status-modal/confirmation-status-modal.module';
-import { HttpClientModule } from '@angular/common/http';
 import { RootLineToasterModule } from '../shared-modules/root-line-toaster/root-line-toaster.module';
 
 const routes: Routes = [
@@ -29,4 +28,4 @@ const routes: Routes = [
     RootLineToasterModule
   ],
 })
-export class AppDashboardModule {}
+export class AppDashboardModule { }

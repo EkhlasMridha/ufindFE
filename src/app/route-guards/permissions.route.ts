5ceb2fc8db29ac4dc917692ea6a8c92ad3058aea
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2021-02-16 03:33:57 +0600

    route guard for role

--- src/app/route-guards/permissions.route.ts
+++ src/app/route-guards/permissions.route.ts
@@ -1,13 +1,28 @@
 const authRoutes: string[] = ['/dashboard'];
 
-const adminRoutes: string[] = ['/admin'];
+const stationRoutes: string[] = ['/submit-case'];
+
+const adminRoutes: string[] = ['/user-create'];
+
+const isAdmin = localStorage.getItem('isAdmin');
 
 export function isAuhtRoute(url: string) {
   let refinedRoute = getRifinedRoute(url);
   console.log('refined url: ' + refinedRoute);
-  let check = authRoutes.includes(refinedRoute);
+  let check = analyzeAccess(isAdmin).includes(refinedRoute);
 
   return check;
+}
+
+function analyzeAccess(isAdmin: string): string[] {
+  let routeList: string[];
+  if (isAdmin == 'true') {
+    routeList = authRoutes.concat(adminRoutes);
+    return routeList;
+  } else {
+    routeList = authRoutes.concat(stationRoutes);
+    return routeList;
+  }
 }
 
 function getRifinedRoute(url: string) {
